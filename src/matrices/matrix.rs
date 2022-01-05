9a592a4246bd9a629bebcd06c0baa1e51f920d02
Lydia Samuel <lydiasamuel3301@gmail.com>
2022-01-05 11:05:21 +0000

    Some more work on matrices

--- src/matrices/matrix.rs
+++ src/matrices/matrix.rs
@@ -1,23 +1,33 @@
-use array2d::Array2D;
+use std::ops::{Mul};
+
+use array2d::{Array2D, Error};
 
 const EPSILON: f64 = 0.00001;
 
 #[derive(Debug, Clone)]
 pub struct Matrix {
-    pub grid: Array2D<f64>
+    pub grid: Array2D<f64>,
 }
 
 impl Matrix {
     pub fn new(num_rows: usize, num_cols: usize) -> Matrix {
         return Matrix {
-            grid: Array2D::filled_with(0.0, num_rows, num_cols)
+            grid: Array2D::filled_with(0.0, num_rows, num_cols),
         };
     }
 
     pub fn from_rows(rows: &Vec<Vec<f64>>) -> Matrix {
         return Matrix {
-            grid: Array2D::from_rows(rows)
+            grid: Array2D::from_rows(rows),
         };
+    }
+
+    pub fn num_rows(&self) -> usize {
+        return self.num_rows();
+    }
+
+    pub fn num_cols(&self) -> usize {
+        return self.num_cols();
     }
 }
 
@@ -42,5 +52,36 @@ impl PartialEq for Matrix {
         }
 
         return true;
+    }
+}
+
+impl Mul for Matrix {
+    type Output = Self;
+
+    fn mul(&self, rhs: Matrix) -> Result<Self, Error> {
+        if (self.num_rows() != rhs.num_cols()) {
+            panic!();
+        }
+        else if (self.num_cols() != rhs.num_rows()) {
+            panic!();
+        }
+
+        let mut row: usize = 0;
+        let mut col: usize = 0;
+
+        for row_iter in other.grid.rows_iter() {
+            col = 0;
+
+            for other_number in row_iter {
+                let self_number = self.grid[(row, col)];
+
+
+
+                col += 1;
+            }
+            row += 1;
+        }
+
+        return Ok();
     }
 }
